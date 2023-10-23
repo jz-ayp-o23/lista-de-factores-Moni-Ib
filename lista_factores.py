51c101275f027baee86dc7c246a4da2afdb78d0d
Moni-ib <monica.ibarra@iteso.mx>
2023-10-23 16:47:16 -0600

    p1

--- lista_factores.py
+++ lista_factores.py
@@ -3,13 +3,14 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = valor
+numeros = []
 
 # Entradas
-entrada = input()
+entrada = int(input("¿De qué número desea los factores? "))
 
 # Proceso
+for numero in range(1, entrada + 1):
+    if entrada % numero == 0:
+        numeros.append(numero)
+print(f"Los factores de {entrada} son: {numeros}")
 
-
-# Salidas
-print(salida)
